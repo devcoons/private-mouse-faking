36daee9c4f3d3c408a155952df9d7503fb8d560a
devcoons <i_-_-_s@outlook.com>
2019-04-25 14:15:06 +0200

    [Released in Public] - Initial working version

---
+++ Mouse Faking/MainWindow.xaml.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mouse_Faking
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Canvas self;
+        public static bool isActive = false;
+        Thread x = new Thread(new ThreadStart(ThreadMouse));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            self = paintSurface;
+            isActive = true;
+            x.Start();
+        }
+        public static Rect GetAbsolutePlacement(FrameworkElement element, bool relativeToScreen = false)
+        {
+            var absolutePos = element.PointToScreen(new System.Windows.Point(0, 0));
+            if (relativeToScreen)
+            {
+                return new Rect(absolutePos.X, absolutePos.Y, element.ActualWidth, element.ActualHeight);
+            }
+            var posMW = Application.Current.MainWindow.PointToScreen(new System.Windows.Point(0, 0));
+            absolutePos = new System.Windows.Point(absolutePos.X - posMW.X, absolutePos.Y - posMW.Y);
+            return new Rect(absolutePos.X, absolutePos.Y, element.ActualWidth, element.ActualHeight);
+        }
+
+        public static void ThreadMouse()
+        {
+            Win32.POINT lpPoint = Win32.GetCursorPosition();
+            Win32.POINT lpPointN = lpPoint;
+
+            int x = 0;
+            int y = 0;
+            bool p = false;
+            bool q = false;
+
+            int k = 0;
+
+            while (isActive)
+            {
+
+                System.Windows.WindowState currentState = WindowState.Normal;
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    currentState = Application.Current.MainWindow.WindowState;
+                });
+
+                if (currentState == WindowState.Minimized)
+                {
+                    Thread.Sleep(1000);
+                    if (isActive == false)
+                        return;
+                    continue;
+                }
+
+                lpPointN.x = lpPoint.x + (p == false ? 1 : -1);
+                lpPointN.y = lpPoint.y + (q == false ? 1 : -1);
+                Rect o = new Rect();
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    o = GetAbsolutePlacement(self, true);
+                });
+
+                if (lpPointN.x < (o.Left + 5))
+                {
+                    lpPointN.x = (int)(o.Left + 5);
+                    p = false;
+
+                }
+                if (lpPointN.y < (o.Top + 5))
+                {
+                    lpPointN.y = (int)(o.Top + 5);
+                    q = false;
+
+                }
+
+
+                if (lpPointN.x > (o.Right - 5))
+                {
+                    lpPointN.x = (int)(o.Right - 5);
+                    p = true;
+
+                }
+                if (lpPointN.y > (o.Bottom - 5))
+                {
+                    lpPointN.y = (int)(o.Bottom - 5);
+                    q = true;
+
+                }
+                Win32.SetCursorPos(lpPointN.x, lpPointN.y);
+                lpPoint = Win32.GetCursorPosition();
+
+                Thread.Sleep(50);
+                if (isActive == false)
+                    return;
+                Win32.POINT lpPointQ = Win32.GetCursorPosition();
+
+                if (Math.Abs(lpPointQ.x - lpPointN.x) > 2 || Math.Abs(lpPointQ.y - lpPointN.y) > 2)
+                {
+                    do
+                    {
+                        Win32.POINT lpPointPre = Win32.GetCursorPosition();
+                        for (int i = 0; i < 10; i++)
+                        {
+                            if (isActive == false)
+                                return;
+                            Thread.Sleep(500);
+                            x = 0;
+                            y = 0;
+                            k = 0;
+                            p = false;
+                            q = false;
+                        }
+                        lpPoint = Win32.GetCursorPosition();
+
+                        if (Math.Abs(lpPoint.x - lpPointPre.x) < 3 && Math.Abs(lpPoint.y - lpPointPre.y) < 3)
+                            break;
+
+                    } while (true);
+                }
+
+                byte[] qp = new byte[1];
+                rngCsp.GetBytes(qp);
+
+                bool mHor = lpPointN.x < (int)(o.Left + o.Right) / 2 ? false : true;
+                bool mVer = lpPointN.y < (int)(o.Top + o.Bottom) / 2 ? false : true;
+
+
+                if (qp[0] < 10)
+                {
+                    Thread.Sleep(10);
+                    rngCsp.GetBytes(qp);
+                    p = qp[0] > 200 ? !mHor : mHor;
+                }
+                rngCsp.GetBytes(qp);
+
+                if (qp[0] < 10)
+                {
+                    Thread.Sleep(10);
+                    rngCsp.GetBytes(qp);
+                    q = qp[0] > 200 ? !mVer : mVer;
+                }
+
+
+                rngCsp.GetBytes(qp);
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    currentState = Application.Current.MainWindow.WindowState;
+                });
+
+                if (currentState == WindowState.Minimized)
+                {
+                    Thread.Sleep(1000);
+                    if (isActive == false)
+                        return;
+                    continue;
+                }
+                k = k < 100 ? k + 1 : 100;
+                if (k > 10)
+                {
+                    if (qp[0] < 50)
+                    {
+                        Win32.sendMouseLeftclick(lpPointN);
+                    }
+                    if (qp[0] > 200)
+                    {
+                        Win32.sendMouseRightclick(lpPointN);
+                    }
+                }
+                if (isActive == false)
+                    return;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            isActive = false;
+            x.Join();
+        }
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+        private void PaintSurface_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                var currentPoint = e.GetPosition(this);
+
+                Line line = new Line();
+
+                byte[] a = new byte[1];
+                byte[] r = new byte[1];
+                byte[] g = new byte[1];
+                byte[] b = new byte[1];
+
+                rngCsp.GetBytes(a);
+                rngCsp.GetBytes(r);
+                rngCsp.GetBytes(g);
+                rngCsp.GetBytes(b);
+
+
+                line.Stroke = new SolidColorBrush(Color.FromArgb(a[0], r[0], g[0], b[0]));
+                line.X1 = currentPoint.X - 1;
+                line.Y1 = currentPoint.Y - 1;
+                line.X2 = e.GetPosition(this).X + 1;
+                line.Y2 = e.GetPosition(this).Y + 1;
+
+                currentPoint = e.GetPosition(this);
+
+                paintSurface.Children.Add(line);
+                Line line1 = new Line();
+                line1.Stroke = new SolidColorBrush(Color.FromArgb(a[0], r[0], g[0], b[0]));
+                line1.X1 = currentPoint.X - 1;
+                line1.Y1 = currentPoint.Y + 1;
+                line1.X2 = e.GetPosition(this).X + 1;
+                line1.Y2 = e.GetPosition(this).Y - 1;
+                paintSurface.Children.Add(line1);
+            }
+        }
+
+    }
+}
---
+++ Mouse Faking/Win32.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mouse_Faking
+{
+
+    public class Win32
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, UIntPtr dwExtraInfo);
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const uint MOUSEEVENTF_LEFTUP = 0x04;
+        private const uint MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const uint MOUSEEVENTF_RIGHTUP = 0x10;
+
+        [DllImport("User32.Dll")]
+        public static extern long SetCursorPos(int x, int y);
+
+        [DllImport("User32.Dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        public static POINT GetCursorPosition()
+        {
+            GetCursorPos(out POINT lpPoint);
+            return lpPoint;
+        }
+
+        [DllImport("User32.Dll")]
+        public static extern bool ClientToScreen(IntPtr hWnd, ref POINT point);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        public static void sendMouseRightclick(POINT p)
+        {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, (uint)p.x, (uint)p.y, 0, (UIntPtr)0);
+        }
+
+        public static void sendMouseLeftclick(POINT p)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)p.x, (uint)p.y, 0, (UIntPtr)0);
+        }
+
+        public static void sendMouseDoubleClick(POINT p)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)p.x, (uint)p.y, 0, (UIntPtr)0);
+
+            Thread.Sleep(150);
+
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)p.x, (uint)p.y, 0, (UIntPtr)0);
+        }
+
+        public static void sendMouseRightDoubleClick(POINT p)
+        {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, (uint)p.x, (uint)p.y, 0, (UIntPtr)0);
+
+            Thread.Sleep(150);
+
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, (uint)p.x, (uint)p.y, 0, (UIntPtr)0);
+        }
+
+        public static void sendMouseDown()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 50, 50, 0, (UIntPtr)0);
+        }
+
+        public static void sendMouseUp()
+        {
+            mouse_event(MOUSEEVENTF_LEFTUP, 50, 50, 0, (UIntPtr)0);
+        }
+    }
+
+}
